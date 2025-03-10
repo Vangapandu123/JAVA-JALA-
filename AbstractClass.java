16130c6c316e5278bb0b6e70eef2d5167423e515
Vangapandu123 <126371950+Vangapandu123@users.noreply.github.com>
2025-03-10 10:08:20 +0530

    AbstractClass.java

--- AbstractClass.java
+++ AbstractClass.java
@@ -18,30 +18,21 @@ class Circle extends Shape {
 class Rectangle extends Shape {  
     private double length;  
     private double width;  
-
     public Rectangle(double length, double width) {  
         this.length = length;  
         this.width = width;  
     }  
-
  @Override  
     public double calculateArea() {  
         return length * width;  
     }  
-}  
-
-  
+}    
 public class AbstractClass {  
     public static void main(String[] args) {  
-     
         Circle circle = new Circle(5);  
         Rectangle rectangle = new Rectangle(4, 6);  
-
-        
         circle.displayInfo();  
         System.out.println("Area of the circle: " + circle.calculateArea());  
-
-        
         rectangle.displayInfo();  
         System.out.println("Area of the rectangle: " + rectangle.calculateArea());  
     }  
