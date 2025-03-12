9167ca79d8e1ff910363ef48dbf7f1ecd070adfd
Vangapandu123 <126371950+Vangapandu123@users.noreply.github.com>
2025-03-12 10:35:46 +0530

    StringConcatenation.java

---
+++ strings/StringConcatenation.java
@@ -0,0 +1,13 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        // Declare two strings
+        String str1 = "Hello";
+        String str2 = "World";
+
+        // Concatenating using + operator
+        String result = str1 + " " + str2;
+
+        // Print the result
+        System.out.println("Concatenated String: " + result);
+    }
+}
