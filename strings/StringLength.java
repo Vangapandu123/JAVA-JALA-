027d68dd0e5d5c01ae28f370bfbc9d8f68cbdfec
Vangapandu123 <126371950+Vangapandu123@users.noreply.github.com>
2025-03-12 10:37:22 +0530

    StringLength.java

---
+++ strings/StringLength.java
@@ -0,0 +1,12 @@
+public class StringLength {
+    public static void main(String[] args) {
+        // Declare a string
+        String str = "Hello, World!";
+
+        // Find the length of the string
+        int length = str.length();
+
+        // Print the result
+        System.out.println("The length of the string is: " + length);
+    }
+}
